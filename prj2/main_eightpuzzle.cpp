58c2eb0fce3ae19db696bc78aeb1f48f69218c38
Wang Xinglu <907682447@qq.com>
2017-04-01 22:01:23 +0800

    AStar for 8 puzzle using set rather than vector

---
+++ prj2/main_eightpuzzle.cpp
@@ -0,0 +1,35 @@
+#include "eightpuzzle.hpp"
+#include <vector>
+#include <iostream>
+using namespace std;
+int main()
+{
+	EightPuzzleState iniState;
+	generateState(&iniState,10); // randomly generate a initial state, the second variable indicates how 
+								// far the initial state is from final state, for debugging, choose 10.
+								// For testing, you may choose 50 - 150. 
+	vector<int> moves;
+	std::string name = "nemo";
+	int age = 1000;
+	std::ostringstream out;
+	out << "name: " << name << ", age: " << age;
+	std::cout << out.str() << '\n';
+	return 0;
+
+	AStarSearchFor8Puzzle(iniState, moves);
+
+	int status = runMoves(&iniState, moves);
+	switch (status)
+	{
+	case 1:
+		cout << " Your solution is correct ! " << endl;
+		break;
+	case -1:
+		cout << " Your solution can not turn initial state to final state !" << endl;
+		break;
+	case -2:
+		cout << " Your solution violates game rule (move blocks out of boundary)!";
+	}
+	printMoves(&iniState, moves);
+	getchar();
+}
